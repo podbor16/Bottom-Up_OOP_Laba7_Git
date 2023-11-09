7f0eb4d54a2089cc3ffb1f7e9209dcee4ea75a9d
podbor16 <podbor250718@gmail.com>
2023-11-09 15:20:38 +0700

    Create empty classes

---
+++ laba2_Korolev.py
@@ -0,0 +1,15 @@
+class Ship:
+    def __init__(self):
+        return
+
+class Steamship(Ship):
+    def __init__(self):
+        return
+
+class SailingShip(Ship):
+    def __init__(self):
+        return
+
+class Corvette(Ship):
+    def __init__(self):
+        return
